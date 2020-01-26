e1f60ae61d95894d0e98f48b9782ee55b1ba698e
Jo <jo3cyc@gmail.com>
2020-01-26 17:39:53 +1100

    Finished lesson

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+
+  { path: '**', redirectTo: ''}
+]
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes, { enableTracing: true})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,58 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+
+import {
+  MatButtonModule,
+  MatButtonToggleModule,
+  // MatIconModule,
+  // MatProgressSpinnerModule,
+  MatToolbarModule,
+  MatSliderModule,
+  MatCardModule,
+  MatInputModule,
+  MatFormFieldModule
+} from '@angular/material';
+
+const material = [
+  MatButtonModule,
+  MatButtonToggleModule,
+  // MatIconModule,
+  // MatProgressSpinnerModule,
+  MatToolbarModule,
+  MatSliderModule,
+  MatCardModule,
+  MatInputModule,
+  MatFormFieldModule
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    material
+  ],
+  exports: [
+    material
   ],
   providers: [],
   bootstrap: [AppComponent]
